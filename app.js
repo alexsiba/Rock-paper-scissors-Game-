50b30da0616a4a53decc3361f32839ea558955fd
Alex <bamlaksi7@gmail.com>
2025-02-08 21:03:06 +0300

    adding live background for more interaction when playing

---
+++ app.js
@@ -0,0 +1,23 @@
+// for the background
+function randomValues() {
+  anime({
+    targets: ".square, .circle, .triangle",
+    translateX: function () {
+      return anime.random(-500, 500);
+    },
+    translateY: function () {
+      return anime.random(-300, 300);
+    },
+    rotate: function () {
+      return anime.random(0, 360);
+    },
+    scale: function () {
+      return anime.random(0.2, 2);
+    },
+    duration: 5000,
+    easing: "easeInOutQuad",
+    complete: randomValues,
+  });
+}
+
+randomValues();
